5e9e1b63501ec627abf0c83923ed1b621cfa5215
Daniele Megna <megna.dany@gmail.com>
2020-12-05 10:38:51 +0100

    Slow improvable solution for day 1 second part

--- lib/advent1.ex
+++ lib/advent1.ex
@@ -1,11 +1,11 @@
 defmodule Advent1 do
 
-  def resolve do
+  def resolve_first_part do
     read_expense_report_from_file()
-    |> resolve_with()
+    |> resolve_first_part_with()
   end
 
-  def resolve_with(expense_report_entries) do
+  def resolve_first_part_with(expense_report_entries) do
     [x, y] = expense_report_entries
     |> candidate_pairs_stream()
     |> find_pair_with_2020_sum()
@@ -13,16 +13,48 @@ defmodule Advent1 do
     x * y
   end
 
+  def resolve_second_part do
+    read_expense_report_from_file()
+    |> resolve_second_part_with()
+  end
+
+  def resolve_second_part_with(expense_report_entries) do
+    [x, y, z] = expense_report_entries
+    |> candidate_tris_stream()
+    |> find_tris_with_2020_sum()
+
+    x * y * z
+  end
+
   defp candidate_pairs_stream(list) do
     list |> Stream.flat_map(fn x ->
       for y <- list, x != y, do: [x, y]
     end)
-    
   end
 
   defp find_pair_with_2020_sum(list) do
     list |> Enum.find(fn [x, y] ->
       x + y == 2020
+    end)
+  end
+
+  defp candidate_tris_stream(list) do
+    list |> Stream.flat_map(fn x ->
+      list |> Stream.flat_map(fn y ->
+        for z <- list,
+          x != y,
+          x != z,
+          y != z
+        do
+          [x, y, z]
+        end
+      end)
+    end)
+  end
+
+  defp find_tris_with_2020_sum(list) do
+    list |> Enum.find(fn [x, y, z] ->
+      x + y + z == 2020
     end)
   end
 
--- test/advent1_test.exs
+++ test/advent1_test.exs
@@ -1,13 +1,22 @@
 defmodule Advent1Test do
   use ExUnit.Case
 
-  test "resolve level" do
-    assert Advent1.resolve == 181044
+  test "resolve_first_part_with provided input example" do
+    input = [1721, 979, 366, 299, 675, 1456]
+    assert Advent1.resolve_first_part_with(input) == 514579
   end
 
-  test "resolve with provided input example" do
+  test "resolve level (first part)" do
+    assert Advent1.resolve_first_part == 181044
+  end
+
+  test "resolve_second_part_with provided input example" do
     input = [1721, 979, 366, 299, 675, 1456]
-    assert Advent1.resolve_with(input) == 514579
+    assert Advent1.resolve_second_part_with(input) == 241861950
+  end
+
+  test "resolve level (second part)" do
+    assert Advent1.resolve_second_part == 82660352
   end
 
 end
