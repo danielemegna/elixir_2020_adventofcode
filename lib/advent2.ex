f630b8f4b056093399ef610e3c72472489c785f5
Daniele Megna <daniele.megna@casavo.com>
2020-12-14 08:21:55 +0100

    Introduce RightPasswordPolicy and resolve second part of day 2

--- lib/advent2.ex
+++ lib/advent2.ex
@@ -3,21 +3,22 @@ defmodule PasswordPolicy do
   defstruct @enforce_keys
 end
 
+defmodule RightPasswordPolicy do
+  @enforce_keys [:first_position, :second_position, :chr]
+  defstruct @enforce_keys
+end
+
 defmodule Advent2 do
 
   def resolve_first_part() do
-    entries = read_passwords_from_file()
-    Enum.count(entries, fn entry ->
-      [policy_string, password] = entry
-      |> String.split(":")
-      |> Enum.map(&String.trim/1)
-
-      policy = build_policy(policy_string)
-      satify_policy?(password, policy)
-    end)
+    resolve_using(&build_password_policy/1)
   end
 
-  def satify_policy?(password, policy) do
+  def resolve_second_part() do
+    resolve_using(&build_right_password_policy/1)
+  end
+
+  def satify_policy?(password, %PasswordPolicy{} = policy) do
     occurencies = password
     |> String.graphemes()
     |> Enum.count(& &1 == policy.chr)
@@ -26,13 +27,48 @@ defmodule Advent2 do
       occurencies <= policy.max
   end
 
-  def build_policy(string) do
-    [limits, chr] = string |> String.split()
-    [min, max] = limits |> String.split("-") |> Enum.map(&String.to_integer(&1))
+  def satify_policy?(password, %RightPasswordPolicy{} = policy) do
+    [
+      String.at(password, policy.first_position - 1),
+      String.at(password, policy.second_position - 1)
+    ]
+    |> Enum.count(&(&1 == policy.chr))
+    |> Kernel.==(1)
+  end
+
+  def build_password_policy(string) do
+    {min, max, chr} = password_policy_pieces(string)
     %PasswordPolicy{ min: min, max: max, chr: chr }
   end
 
-  defp read_passwords_from_file do
+  def build_right_password_policy(string) do
+    {first_position, second_position, chr} = password_policy_pieces(string)
+    %RightPasswordPolicy{ first_position: first_position, second_position: second_position, chr: chr }
+  end
+
+  defp resolve_using(build_password_policy_fn) do
+    read_password_entries_from_file()
+    |> Enum.map(&parse_entry/1)
+    |> Enum.count(fn {policy_string, password} ->
+      policy = build_password_policy_fn.(policy_string)
+      password |> satify_policy?(policy)
+    end)
+  end
+
+  defp parse_entry(entry) do
+    entry
+    |> String.split(":")
+    |> Enum.map(&String.trim/1)
+    |> List.to_tuple
+  end
+
+  defp password_policy_pieces(string) do
+    [range, chr] = string |> String.split()
+    [first, second] = range |> String.split("-") |> Enum.map(&String.to_integer(&1))
+    {first, second, chr}
+  end
+
+  defp read_password_entries_from_file do
     File.stream!("advent2.txt")
     |> Stream.map(&String.trim/1)
   end
--- test/advent2_test.exs
+++ test/advent2_test.exs
@@ -5,6 +5,10 @@ defmodule Advent2Test do
     assert Advent2.resolve_first_part() == 625
   end
 
+  test "resolve second part" do
+    assert Advent2.resolve_second_part() == 391
+  end
+
   test "password satify_policy?" do
     policy = %PasswordPolicy{ min: 1, max: 3, chr: "a" }
     assert Advent2.satify_policy?("abcde", policy) == true
@@ -13,15 +17,38 @@ defmodule Advent2Test do
     policy = %PasswordPolicy{ min: 2, max: 9, chr: "c" }
     assert Advent2.satify_policy?("c", policy) == false
     assert Advent2.satify_policy?("cc", policy) == true
+    assert Advent2.satify_policy?("czzzzzzzz", policy) == false
+    assert Advent2.satify_policy?("zzzzzzzzc", policy) == false
     assert Advent2.satify_policy?("czzzzzzzc", policy) == true
     assert Advent2.satify_policy?("ccccccccc", policy) == true
     assert Advent2.satify_policy?("cccccccccc", policy) == false
   end
 
-  test "build_policy from string" do
-    assert Advent2.build_policy("1-3 a") == %PasswordPolicy{ min: 1, max: 3, chr: "a" }
-    assert Advent2.build_policy("0-3 b") == %PasswordPolicy{ min: 0, max: 3, chr: "b" }
-    assert Advent2.build_policy("2-9 c") == %PasswordPolicy{ min: 2, max: 9, chr: "c" }
+  test "build_password_policy from string" do
+    assert Advent2.build_password_policy("1-3 a") == %PasswordPolicy{ min: 1, max: 3, chr: "a" }
+    assert Advent2.build_password_policy("0-3 b") == %PasswordPolicy{ min: 0, max: 3, chr: "b" }
+    assert Advent2.build_password_policy("2-9 c") == %PasswordPolicy{ min: 2, max: 9, chr: "c" }
+  end
+  
+  test "build_right_password_policy from string" do
+    assert Advent2.build_right_password_policy("1-3 a") == %RightPasswordPolicy{ first_position: 1, second_position: 3, chr: "a" }
+    assert Advent2.build_right_password_policy("0-3 b") == %RightPasswordPolicy{ first_position: 0, second_position: 3, chr: "b" }
+    assert Advent2.build_right_password_policy("2-9 c") == %RightPasswordPolicy{ first_position: 2, second_position: 9, chr: "c" }
+  end
+
+  test "password satify_policy? with RightPasswordPolicy" do
+    policy = %RightPasswordPolicy{ first_position: 1, second_position: 3, chr: "a" }
+    assert Advent2.satify_policy?("abcde", policy) == true
+    assert Advent2.satify_policy?("cdefg", policy) == false
+
+    policy = %RightPasswordPolicy{ first_position: 2, second_position: 9, chr: "c" }
+    assert Advent2.satify_policy?("czzzzzzzz", policy) == false
+    assert Advent2.satify_policy?("cczzzzzzz", policy) == true
+    assert Advent2.satify_policy?("zzzzzzzzc", policy) == true
+    assert Advent2.satify_policy?("zczzzzzzc", policy) == false
+    assert Advent2.satify_policy?("czccccccz", policy) == false
+    assert Advent2.satify_policy?("ccccccccz", policy) == true
+    assert Advent2.satify_policy?("ccccccccc", policy) == false
   end
 
 end
