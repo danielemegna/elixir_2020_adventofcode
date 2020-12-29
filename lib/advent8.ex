d6875f57b1d702b3521b9f4709471ea7d24e7b2b
Daniele Megna <megna.dany@gmail.com>
2020-12-29 11:21:43 +0100

    First walking skeleton for Advent8: the Machine module

---
+++ lib/advent8.ex
@@ -0,0 +1,36 @@
+defmodule Machine do
+  defstruct [pointer: 0, acc: 0]
+
+  def run(%Machine{} = m, instructions) when is_list(instructions) do
+    i = Enum.at(instructions, m.pointer)
+    m = run(m, i)
+
+    if should_halt(m, instructions), do: m,
+    else: run(m, instructions)
+  end
+
+  def run(%Machine{} = m, {:acc, v}), do:
+    %{m | acc: m.acc+v, pointer: m.pointer+1}
+
+  def run(%Machine{} = m, {:jmp, v}), do:
+    %{m | pointer: m.pointer+v}
+  
+  def run(%Machine{} = m, {:nop, _}), do:
+    %{m | pointer: m.pointer+1}
+
+  defp should_halt(%Machine{} = m, instructions) do
+    m.pointer < 0 || m.pointer >= Enum.count(instructions)
+  end
+
+end
+
+defmodule Advent8 do
+  
+  def parse_instructions(stream) do
+    Enum.map(stream, fn line ->
+      [op, val] = String.split(line)
+      {String.to_atom(op), String.to_integer(val)}
+    end)
+  end
+
+end
---
+++ test/advent8_test.exs
@@ -0,0 +1,50 @@
+defmodule Advent8Test do
+  use ExUnit.Case
+
+  @loop_example_program """
+  nop +0
+  acc +1
+  jmp +4
+  acc +3
+  jmp -3
+  acc -99
+  acc +1
+  jmp -4
+  acc +6
+  """
+
+  defp stream_of(content), do: content |> String.split("\n", trim: true) |> Stream.map(&(&1))
+
+  test "parse instructions" do
+    actual = Advent8.parse_instructions(stream_of(@loop_example_program))
+    assert actual == [
+      {:nop, 0}, {:acc, 1}, {:jmp, 4},
+      {:acc, 3}, {:jmp, -3}, {:acc, -99},
+      {:acc, 1}, {:jmp, -4}, {:acc, 6}
+    ]
+  end
+
+  test "single acc instruction program" do
+    m = %Machine{}
+    assert m == %Machine{pointer: 0, acc: 0}
+
+    m = Machine.run(m, [{:acc, 2}])
+    assert m == %Machine{pointer: 1, acc: 2}
+  end
+
+  test "handle jmp instruction" do
+    m = Machine.run(%Machine{}, [{:jmp, 4}])
+    assert m == %Machine{pointer: 4, acc: 0}
+
+    m = Machine.run(%Machine{}, [{:jmp, -8}])
+    assert m == %Machine{pointer: -8, acc: 0}
+  end
+  
+  test "handle nop instruction" do
+    m = Machine.run(%Machine{}, [{:nop, 0}])
+    assert m == %Machine{pointer: 1, acc: 0}
+
+    m = Machine.run(%Machine{}, [{:nop, 99}])
+    assert m == %Machine{pointer: 1, acc: 0}
+  end
+end
