86312477eba6aa78be0c4ad16ec85a6bf2cf5190
Daniele Megna <megna.dany@gmail.com>
2021-01-02 09:09:00 +0100

    Day 9 second part solved !
    Very long processing btw ... tagged as skipped test

--- lib/advent9.ex
+++ lib/advent9.ex
@@ -6,6 +6,15 @@ defmodule Advent9 do
     |> find_first_invalid(25)
   end
 
+  def resolve_second_part() do
+    sequence = read_xmas_data()
+    |> Stream.map(&String.to_integer/1)
+    
+    # optimization knowing first part solution
+    # find_encryption_weakness(sequence, find_first_invalid(sequence, 25))
+    find_encryption_weakness(sequence, 556543474)
+  end
+
   def find_first_invalid(sequence, window_size) do
     preamble = sequence |> Enum.take(window_size)
     to_validate = sequence |> Enum.at(window_size)
@@ -14,6 +23,30 @@ defmodule Advent9 do
       to_validate
     else
       find_first_invalid(Stream.drop(sequence, 1), window_size)
+    end
+  end
+
+  def find_encryption_weakness(sequence, target) do
+    sequence
+    |> accumulate_to_target(%{total: 0, min: nil, max: -1}, target)
+    |> case do
+      :overflow -> find_encryption_weakness(Stream.drop(sequence, 1), target)
+      acc -> acc.min + acc.max
+    end
+  end
+
+  defp accumulate_to_target(stream, acc, target) do
+    n = Enum.at(stream, 0)
+    acc = %{acc |
+      total: acc.total + n,
+      min: min(acc.min, n),
+      max: max(acc.max, n)
+    }
+
+    cond do
+      acc.total > target -> :overflow
+      acc.total == target -> acc
+      true -> accumulate_to_target(Stream.drop(stream, 1), acc, target)
     end
   end
 
--- test/advent9_test.exs
+++ test/advent9_test.exs
@@ -6,6 +6,12 @@ defmodule Advent9Test do
     assert Advent9.resolve_first_part() == 556543474
   end
 
+  @tag :skip
+  @tag timeout: :infinity
+  test "resolve second part" do
+    assert Advent9.resolve_second_part() == 76096372
+  end
+
   test "find first invalid with provided example" do
     sequence = [
       35, 20, 15, 25, 47,
@@ -16,4 +22,16 @@ defmodule Advent9Test do
 
     assert Advent9.find_first_invalid(sequence, window_size) == 127
   end
+
+  test "find_encryption_weakness number with provided example" do
+    sequence = [
+      35, 20, 15, 25, 47, 40,
+      62, 55, 65, 95, 102, 117, 150,
+      182, 127, 219, 299, 277, 309, 576
+    ] |> Stream.map(&(&1))
+    target = 127
+
+    assert Advent9.find_encryption_weakness(sequence, target) == 15 + 47
+  end
+
 end
