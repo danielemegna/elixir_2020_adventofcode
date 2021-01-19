756f3a8ee8b318d74108864c0b5e96010e248705
Daniele Megna <daniele.megna@casavo.com>
2021-01-19 14:48:18 +0100

    Refactor and improve names in day 10 final solution

--- lib/advent10.ex
+++ lib/advent10.ex
@@ -9,59 +9,46 @@ defmodule Advent10 do
   def resolve_second_part() do
     read_adapters_list()
     |> Stream.map(&String.to_integer/1)
-    |> arrange_combinations_count()
+    |> combinations_count()
  end
 
   def differences_calc(seq) do
     differences = seq
     |> connect_all()
-    |> differences_in()
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [x,y] -> y-x end)
 
     Enum.count(differences, &(&1 == 1)) *
       Enum.count(differences, &(&1 == 3))
   end
 
-  def arrange_combinations_count(seq) do
-    {_, [{0, return} | _]} = seq
+  def combinations_count(seq) do
+    seq
     |> connect_all()
     |> Enum.reverse
-    |> Enum.reduce({[], []}, fn(n, {list, results}) ->
-      new_list = list ++ [n]
-      result = foooo(new_list, results)
-      {new_list, [{n, result} | results]}
+    |> Enum.reduce([], fn(n, results) ->
+      [{n, combinations_count(n, results)} | results]
     end)
-
-    return
+    |> Enum.at(0)
+    |> elem(1)
   end
 
-  defp foooo([_], _), do: 1
+  defp combinations_count(_, []), do: 1
 
-  defp foooo(list, results) do
-    [head | _] = list |> Enum.reverse()
-
+  defp combinations_count(n, results) do
     results
     |> Enum.take(3)
-    |> Enum.reduce(0, fn {k,v}, acc ->
-      if(k-head <= 3) do
-        acc + v
-      else
-        acc
-      end
+    |> Enum.reduce(0, fn
+      {k,v}, acc when(k-n <= 3) -> acc + v
+      _, acc -> acc
     end)
   end
-
 
   defp connect_all(seq) do
     seq
     |> Enum.sort()
     |> List.insert_at(0, 0) # charging outlet
     |> List.insert_at(-1, Enum.max(seq) + 3) # device's built-in adapter
-  end
-
-  defp differences_in(seq) do
-    seq
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.map(fn [x,y] -> y-x end)
   end
 
   defp read_adapters_list() do
--- test/advent10_test.exs
+++ test/advent10_test.exs
@@ -34,17 +34,17 @@ defmodule Advent10Test do
     # (0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
     # (0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
     # (0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
-    assert Advent10.arrange_combinations_count([16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]) == 8
+    assert Advent10.combinations_count([16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]) == 8
   end
 
   test "single possibile combination" do
-    assert Advent10.arrange_combinations_count([3,6,9,12,15]) == 1
+    assert Advent10.combinations_count([3,6,9,12,15]) == 1
   end
   
   test "two possibile combination" do
     # 3 6 7 8 11
     # 3 6 8 11
-    assert Advent10.arrange_combinations_count([3,6,7,8,11]) == 2
+    assert Advent10.combinations_count([3,6,7,8,11]) == 2
   end
 
   test "4 possibile combination" do
@@ -52,7 +52,7 @@ defmodule Advent10Test do
     # 3 5 6
     # 3 6
     # 3 4 6
-    assert Advent10.arrange_combinations_count([3,4,5,6]) == 4
+    assert Advent10.combinations_count([3,4,5,6]) == 4
   end
 
   test "n possibile combination" do
@@ -66,7 +66,7 @@ defmodule Advent10Test do
     # (0) 3 5 7 (10)
     #
     # (0) 3 6 7 (10)
-    assert Advent10.arrange_combinations_count([3,4,5,6,7]) == 7
+    assert Advent10.combinations_count([3,4,5,6,7]) == 7
   end
 
   test "arrange combinations count on larger provided example" do
@@ -75,7 +75,7 @@ defmodule Advent10Test do
       24, 23, 49, 45, 19, 38, 39, 11, 1, 32,
       25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3
     ]
-    assert Advent10.arrange_combinations_count(input) == 19208
+    assert Advent10.combinations_count(input) == 19208
   end
 
 end
