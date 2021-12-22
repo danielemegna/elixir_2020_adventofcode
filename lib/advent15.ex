fa6059e3e027561b77fb84e266286e2a19645b38
Daniele Megna <daniele.megna@casavo.com>
2021-12-22 08:44:58 +0100

    Day 15 solved ! to be clean and refactored ..

--- lib/advent15.ex
+++ lib/advent15.ex
@@ -9,7 +9,8 @@ defmodule Advent15 do
   end
 
   def last_spoken_number_for(starting_numbers, limit) do
-    spoken_numbers_stack_for(starting_numbers, limit) |> Enum.at(0)
+    #spoken_numbers_stack_for(starting_numbers, limit) |> Enum.at(0)
+    spoken_numbers_map_for(starting_numbers, limit).last_spoken
   end
 
   def spoken_numbers_stack_for(starting_numbers, limit) do
@@ -19,20 +20,52 @@ defmodule Advent15 do
     )
   end
 
-  defp produce_spoken_numbers_stack(stack, 0), do: stack
-  defp produce_spoken_numbers_stack(stack, remaining_work) do
-    next = next_for(stack)
-    new_stack = [next | stack]
-
-    produce_spoken_numbers_stack(new_stack, remaining_work-1)
+  def spoken_numbers_map_for(starting_numbers, limit) do
+    map = init_spoken_numbers_map(starting_numbers)
+    produce_spoken_numbers_map(map, limit - Enum.count(starting_numbers))
   end
 
-  def next_for([head | tail]) do
+  defp produce_spoken_numbers_map(map, 0), do: map
+  defp produce_spoken_numbers_map(map, remaining_work) do
+    next = next_for(map)
+    new_map = map
+      |> Map.put(:already_spoken, Map.put(map.already_spoken, map.last_spoken, map.current_turn))
+      |> Map.put(:last_spoken, next)
+      |> Map.put(:current_turn, map.current_turn+1)
+    produce_spoken_numbers_map(new_map, remaining_work-1)
+  end
+
+  def next_for([head | tail] = stack) when is_list(stack) do
     Enum.find_index(tail, &(&1 == head))
     |> case do
       nil -> 0
       index -> index + 1
     end
   end
+
+  def next_for(m) when is_map(m) do
+    if(Map.has_key?(m.already_spoken, m.last_spoken)) do
+      m.current_turn - m.already_spoken[m.last_spoken]
+    else
+      0
+    end
+  end
+
+  defp produce_spoken_numbers_stack(stack, 0), do: stack
+  defp produce_spoken_numbers_stack(stack, remaining_work) do
+    next = next_for(stack)
+    new_stack = [next | stack]
+    produce_spoken_numbers_stack(new_stack, remaining_work-1)
+  end
+
+  defp init_spoken_numbers_map(starting_numbers) do
+    {last_spoken, already_spoken} = List.pop_at(starting_numbers, -1)
+    %{
+      last_spoken: last_spoken,
+      current_turn: Enum.count(starting_numbers),
+      already_spoken: Enum.with_index(already_spoken, 1) |> Map.new
+    }
+  end
+
 
 end
--- test/advent15_test.exs
+++ test/advent15_test.exs
@@ -7,7 +7,7 @@ defmodule Advent15Test do
 
   @tag :skip
   test "resolve second part" do
-    assert Advent15.resolve_second_part() == :todo
+    assert Advent15.resolve_second_part() == 950
   end
 
   describe "last spoken number" do
@@ -34,6 +34,30 @@ defmodule Advent15Test do
 
   end
 
+  describe "spoken_numbers_map_for" do
+
+    test "init with starting numbers" do
+      actual = Advent15.spoken_numbers_map_for([0,3,6], 3)
+      expected = %{
+        last_spoken: 6,
+        current_turn: 3,
+        already_spoken: %{0 => 1, 3 => 2}
+      }
+      assert actual == expected
+    end
+
+    test "short case" do
+      actual = Advent15.spoken_numbers_map_for([0,3,6], 10)
+      expected = %{
+        last_spoken: 0,
+        current_turn: 10,
+        already_spoken: %{0 => 8, 3 => 6, 6 => 3, 1 => 7, 4 => 9}
+      }
+      assert actual == expected
+    end
+
+  end
+
   describe "spoken_numbers_stack" do
 
     test "short case" do
@@ -50,10 +74,37 @@ defmodule Advent15Test do
       assert Advent15.next_for([1,3,3,0,6,3,0]) == 0
     end
 
-    test "already met numbers: the different between current turn and position where most recently met" do
+    test "already met numbers: the difference between current turn and position where most recently met" do
       assert Advent15.next_for([0,6,3,0]) == (4-1)
       assert Advent15.next_for([3,0,6,3,0]) == (5-2)
       assert Advent15.next_for([0,1,3,3,0,6,3,0]) == (8-4)
+    end
+
+  end
+
+  describe "generate next for spoken numbers memory" do
+
+    test "zero for never met numbers" do
+      assert Advent15.next_for(%{last_spoken: 9, current_turn: 1, already_spoken: %{}}) == 0
+      assert Advent15.next_for(%{last_spoken: 6, current_turn: 3, already_spoken: %{0 => 1, 3 => 2}}) == 0
+    end
+
+    test "already met numbers: the difference between current turn and position where most recently met" do
+      assert Advent15.next_for(%{
+        last_spoken: 0,
+        current_turn: 4,
+        already_spoken: %{0 => 1, 3 => 2, 6 => 3}
+      }) == (4-1)
+      assert Advent15.next_for(%{
+        last_spoken: 3,
+        current_turn: 5,
+        already_spoken: %{0 => 4, 3 => 2, 6 => 3}
+      }) == (5-2)
+      assert Advent15.next_for(%{
+        last_spoken: 0,
+        current_turn: 8,
+        already_spoken: %{0 => 4, 3 => 6, 6 => 3, 1 => 7}
+      }) == (8-4)
     end
 
   end
