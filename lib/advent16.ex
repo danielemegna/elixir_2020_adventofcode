3e63f466a46c7a454b77e8c6ad56f3dfd589a579
Daniele Megna <daniele.megna@casavo.com>
2021-12-28 07:59:27 +0100

    Minor refactor in Advent16

--- lib/advent16.ex
+++ lib/advent16.ex
@@ -11,6 +11,20 @@ defmodule Advent16 do
     |> error_rate_of()
   end
 
+  def error_rate_of(%Advent16.File{} = file) do
+    rules_ranges = file.rules
+    |> Enum.flat_map(&(&1.ranges))
+
+    file.nearby_tickets
+    |> Enum.map(fn ticket_fields ->
+      invalid_ticket_fields = Enum.filter(ticket_fields, fn field_value ->
+        is_invalid_for_every_rule?(field_value, rules_ranges)
+      end)
+      Enum.sum(invalid_ticket_fields)
+    end)
+    |> Enum.sum
+  end
+
   def parse_input_file(lines) do
     [rules_lines, your_ticket_lines, nearby_tickets_lines] =
       Enum.chunk_by(lines, fn l -> l == "" end) |> Enum.reject(&(&1 == [""]))
@@ -46,25 +60,14 @@ defmodule Advent16 do
     %Advent16.File{rules: rules, your_ticket: your_ticket, nearby_tickets: nearby_tickets}
   end
 
-  def error_rate_of(%Advent16.File{} = file) do
-    rules_ranges = file.rules
-    |> Enum.flat_map(&(&1.ranges))
-
-    file.nearby_tickets
-    |> Enum.map(fn ticket_fields ->
-      invalid_ticket_fields = Enum.filter(ticket_fields, fn field_value ->
-        is_invalid_for_every_rule?(field_value, rules_ranges)
-      end)
-      Enum.sum(invalid_ticket_fields)
-    end)
-    |> Enum.sum
-  end
-
   defp is_invalid_for_every_rule?(field_value, rules_ranges) do
-    Enum.all?(rules_ranges, fn {r_min, r_max} ->
-      field_value < r_min || field_value > r_max
+    Enum.all?(rules_ranges, fn rule_range ->
+      !is_valid_for(field_value, rule_range)
     end)
   end
+
+  defp is_valid_for(field_value, _rule_range = {r_min, r_max}), do:
+    field_value >= r_min && field_value <= r_max
 
   defp line_parse_error(line) do
     raise ArgumentError, message: "Cannot parse input line '#{line}'"
