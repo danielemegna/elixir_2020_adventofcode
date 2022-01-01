76318e69341bd395193bf97feaa52d469e212b94
Daniele Megna <daniele.megna@casavo.com>
2022-01-01 19:21:43 +0100

    Count alive neighbors of a coordinate

--- lib/advent17.ex
+++ lib/advent17.ex
@@ -1,5 +1,56 @@
 defmodule Advent17 do
 
+  def alive_neighbors(alive_cubes, coordinate) do
+    neighbors_coordinates = neighbors_coordinates_of(coordinate)
+    (alive_cubes -- (alive_cubes -- neighbors_coordinates))
+    |> Enum.count
+  end
+
+  defp neighbors_coordinates_of({x,y,z}) do
+    # TODO: generate this ?
+
+    [
+      {x,y-1,z},
+      {x-1,y-1,z},
+      {x+1,y-1,z},
+
+      {x,y+1,z},
+      {x+1,y+1,z},
+      {x-1,y+1,z},
+
+      {x-1,y,z},
+      {x+1,y,z},
+
+      # --------
+
+      {x,y-1,z+1},
+      {x-1,y-1,z+1},
+      {x+1,y-1,z+1},
+
+      {x,y+1,z+1},
+      {x+1,y+1,z+1},
+      {x-1,y+1,z+1},
+
+      {x-1,y,z+1},
+      {x,y,z+1},
+      {x+1,y,z+1},
+
+      # --------
+
+      {x,y-1,z-1},
+      {x-1,y-1,z-1},
+      {x+1,y-1,z-1},
+
+      {x,y+1,z-1},
+      {x+1,y+1,z-1},
+      {x-1,y+1,z-1},
+
+      {x-1,y,z-1},
+      {x,y,z-1},
+      {x+1,y,z-1},
+    ]
+  end
+
   def parse_inital_configuration(lines) do
     lines
     |> Enum.with_index
--- test/advent17_test.exs
+++ test/advent17_test.exs
@@ -19,6 +19,46 @@ defmodule Advent17Test do
     ]
   end
 
+  describe "alive_neighbors" do
+
+    setup do
+      alive_cubes = [
+        {1,0,0},
+        {2,1,0},
+        {0,2,0},
+        {1,2,0},
+        {2,2,0}
+      ]
+      [c: alive_cubes]
+    end
+
+    test "of an alive cube", %{c: alive_cubes} do
+      assert 1 == Advent17.alive_neighbors(alive_cubes, {1,0,0})
+      assert 3 == Advent17.alive_neighbors(alive_cubes, {2,1,0})
+      assert 3 == Advent17.alive_neighbors(alive_cubes, {1,2,0})
+    end
+
+    test "of a dead cube in known limits", %{c: alive_cubes} do
+      assert 3 == Advent17.alive_neighbors(alive_cubes, {0,1,0})
+      assert 5 == Advent17.alive_neighbors(alive_cubes, {1,1,0})
+    end
+
+    test "of a dead cube out known limits in same layer", %{c: alive_cubes} do
+      assert 1 == Advent17.alive_neighbors(alive_cubes, {0,-1,0})
+      assert 3 == Advent17.alive_neighbors(alive_cubes, {1,3,0})
+      assert 0 == Advent17.alive_neighbors(alive_cubes, {-99,99,0})
+    end
+
+    test "of a dead cube in another layer", %{c: alive_cubes} do
+      assert 5 == Advent17.alive_neighbors(alive_cubes, {1,1,-1})
+      assert 5 == Advent17.alive_neighbors(alive_cubes, {1,1,1})
+      assert 3 == Advent17.alive_neighbors(alive_cubes, {1,3,-1})
+      assert 0 == Advent17.alive_neighbors(alive_cubes, {1,1,99})
+      assert 0 == Advent17.alive_neighbors(alive_cubes, {1,1,-99})
+    end
+
+  end
+
   defp stream_of(content), do: content |> String.split("\n") |> Stream.map(&(&1))
 
 end
