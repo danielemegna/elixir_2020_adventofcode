f016b618ff1ea5c2b94912bff93bbf46eb373e54
Daniele Megna <daniele.megna@casavo.com>
2022-01-04 10:49:06 +0100

    Significantly improve 17 first part solution
    - use a MapSet to collect interested cubes for next cycle using
      neighbors_coordinates_of instead of every cube in the 3d known space
    - use (naive) parallel_map in the core next-state-calculation map

--- lib/advent17.ex
+++ lib/advent17.ex
@@ -37,28 +37,25 @@ defmodule Advent17 do
   end
 
   def execute_cycle(active_cubes) do
-    {xs,ys,zs} = active_cubes
-    |> Enum.reduce({[],[],[]}, fn {x,y,z}, {xs,ys,zs} ->
-      { [x | xs], [y | ys], [z | zs] }
+    active_cubes
+    |> Enum.reduce(MapSet.new(), fn coordinate, acc ->
+      MapSet.union(acc, MapSet.new(neighbors_coordinates_of(coordinate)))
     end)
-
-    (Enum.min(xs)-1)..(Enum.max(xs)+1)
-    |> Enum.flat_map(fn x ->
-      (Enum.min(ys)-1)..(Enum.max(ys)+1)
-      |> Enum.flat_map(fn y ->
-        (Enum.min(zs)-1)..(Enum.max(zs)+1)
-        |> Enum.map(fn z ->
-          current = {x,y,z}
-          was_active? = Enum.member?(active_cubes, current)
-          active_neighbors = active_neighbors(active_cubes, current)
-          case next_state(was_active?, active_neighbors) do
-            :active -> current
-            :inactive -> nil
-          end
-        end)
-      end)
+    |> parallel_map(fn current ->
+      was_active? = Enum.member?(active_cubes, current)
+      active_neighbors = active_neighbors(active_cubes, current)
+      case next_state(was_active?, active_neighbors) do
+        :active -> current
+        :inactive -> nil
+      end
     end)
     |> Enum.reject(&is_nil/1)
+  end
+
+  defp parallel_map(collection, func) do
+    collection
+    |> Enum.map(&(Task.async(fn -> func.(&1) end)))
+    |> Enum.map(&Task.await/1)
   end
 
   defp next_state(true, active_neighbors) when active_neighbors in [2,3], do: :active
