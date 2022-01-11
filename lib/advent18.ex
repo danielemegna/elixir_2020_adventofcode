2bac7df5a1648dead0ce0c8c4636cb287521d71a
Daniele Megna <daniele.megna@casavo.com>
2022-01-11 11:24:23 +0100

    Please do not look at this code ...... day 18 solved

--- lib/advent18.ex
+++ lib/advent18.ex
@@ -6,6 +6,12 @@ defmodule Advent18 do
     |> Enum.sum
   end
 
+  def resolve_second_part do
+    read_input_file_content()
+    |> Stream.map(&evaluate_expression_add_precedence/1)
+    |> Enum.sum
+  end
+
   def evaluate_expression(string) do
     string
     |> String.replace("(", "( ")
@@ -33,34 +39,81 @@ defmodule Advent18 do
   defp evaluate([], {accumulated, _}), do: accumulated
 
   defp evaluate([n | rest], {accumulated, next_operation}) do
-    operation_result = calc(accumulated, String.to_integer(n), next_operation)
+    operation_result = calc(accumulated, to_int(n), next_operation)
     evaluate(rest, {operation_result, nil})
   end
+
+  # --------------
+
+  def evaluate_expression_add_precedence(string) do
+    string
+    |> String.replace("(", "( ")
+    |> String.replace(")", " )")
+    |> String.split()
+    |> evaluate_add_precedence()
+  end
+
+  defp evaluate_add_precedence(list) do
+    list
+    |> solve_sums()
+    |> evaluate()
+  end
+
+  defp solve_sums(list) do
+    Enum.find_index(list, &(&1 == "+"))
+    |> case do
+      nil -> list
+      n ->
+        {left, [_plus | right]} = Enum.split(list, n)
+
+        {r_value, r_rest} = case right do
+          ["(" | rest] ->
+            close_parenthesis_index = close_parenthesis_index(rest)
+            inner = Enum.take(rest, close_parenthesis_index)
+            inner_evaluation = evaluate_add_precedence(inner)
+            {inner_evaluation, Enum.drop(rest, close_parenthesis_index+1)}
+          [n | rest] -> 
+            {to_int(n), rest}
+        end
+
+        {l_rest, [l_value]} = Enum.split(left, -1)
+
+        {l_value, l_rest} = case l_value do
+          ")" ->
+            close_parenthesis_index = -(close_parenthesis_index(Enum.reverse(l_rest), "(", ")"))
+            inner = Enum.take(l_rest, close_parenthesis_index)
+            inner_evaluation = evaluate_add_precedence(inner)
+            {inner_evaluation, Enum.drop(l_rest, close_parenthesis_index-1)}
+          n ->
+            {to_int(n), l_rest}
+        end
+
+        sum = l_value + r_value
+        new_list = l_rest ++ [sum] ++ r_rest
+        solve_sums(new_list)
+    end
+  end
+
+  # --------------
 
   defp calc(prev, current, :sum), do: prev + current
   defp calc(prev, current, :prod), do: prev * current
 
-  defp close_parenthesis_index(list) do
+  defp close_parenthesis_index(list, close_char \\ ")", open_char \\ "(") do
     list
     |> Stream.with_index()
     |> Enum.reduce_while(0, fn {c, idx}, nest_level ->
       case {c, nest_level} do
-        {")", 0} -> {:halt, idx}
-        {"(", _} -> {:cont, nest_level+1}
-        {")", _} -> {:cont, nest_level-1}
+        {^close_char, 0} -> {:halt, idx}
+        {^open_char, _} -> {:cont, nest_level+1}
+        {^close_char, _} -> {:cont, nest_level-1}
         _ -> {:cont, nest_level}
       end
     end)
   end
 
-  defp drop_rest([first | rest], nest_level \\ 0) do
-    case {first, nest_level} do
-      {")", 0} -> rest
-      {")", _} -> drop_rest(rest, nest_level-1)
-      {"(", _} -> drop_rest(rest, nest_level+1)
-      _ -> drop_rest(rest, nest_level)
-    end
-  end
+  defp to_int(n) when is_integer(n), do: n
+  defp to_int(b) when is_binary(b), do: String.to_integer(b)
 
   defp read_input_file_content do
     File.stream!("advent18.txt")
--- test/advent18_test.exs
+++ test/advent18_test.exs
@@ -5,6 +5,10 @@ defmodule Advent18Test do
     assert Advent18.resolve_first_part() == 4297397455886
   end
 
+  test "resolve second part" do
+    assert Advent18.resolve_second_part() == 93000656194428
+  end
+
   describe "evaluate_expression" do
     
     test "very simple" do
@@ -29,4 +33,36 @@ defmodule Advent18Test do
     end
 
   end
+
+  describe "evaluate_expression in :add_precedence" do
+
+    test "very simple" do
+      assert Advent18.evaluate_expression_add_precedence("1 + 2") == 3
+      assert Advent18.evaluate_expression_add_precedence("2 * 3") == 6
+      assert Advent18.evaluate_expression_add_precedence("12 * 2") == 24
+      assert Advent18.evaluate_expression_add_precedence("10 + 10") == 20
+    end
+
+    test "complex without parentheses" do
+      assert Advent18.evaluate_expression_add_precedence("1 + 2 * 3 + 4 * 5 + 6") == 231
+    end
+
+    test "with parentheses" do
+      assert Advent18.evaluate_expression_add_precedence("2 * 3 + (4 * 5)") == 46
+    end
+
+    test "complex provided examples" do
+      assert Advent18.evaluate_expression_add_precedence("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 1445
+      assert Advent18.evaluate_expression_add_precedence("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 669060
+      assert Advent18.evaluate_expression_add_precedence("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 23340
+    end
+
+    test "more and more" do
+      assert Advent18.evaluate_expression_add_precedence(
+        "((5 * 7 + 2) * 3 + 4 + 2) * 8 * ((4 + 9 * 7 + 2 * 9 * 3) + 8 * 5 * (6 * 4 + 3) + (3 * 8 * 2)) + 4 + 7 + 3"
+      ) == 4617531360
+    end
+
+  end
+
 end
