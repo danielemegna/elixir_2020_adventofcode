a09aa96bd82dee6d0b60181105468bd609ebd498
Daniele Megna <daniele.megna@casavo.com>
2022-02-11 19:26:20 +0100

    A first dirty solution for matching messages count in day 19!

--- lib/advent19.ex
+++ lib/advent19.ex
@@ -1,5 +1,51 @@
 defmodule Advent19 do
 
+  def count_matches_with_rule(messages, rule_number, rules) do
+    messages
+    |> Enum.count(fn message ->
+      match_with_rule?(message, rule_number, rules)
+    end)
+  end
+
+  def match_with_rule?(message, rule_number, rules) when is_integer(rule_number) do
+    rule = rules[rule_number]
+    match_with_rule?(message, rule, rules)
+  end
+
+  def match_with_rule?(message, [[_|_]|_] = composed_rule, rules) do
+    composed_rule
+    |> Enum.any?(&(match_with_rule?(message, &1, rules)))
+  end
+
+  def match_with_rule?("", [], _), do: true
+  def match_with_rule?(_, [], _), do: false
+  def match_with_rule?(message, rule, rules) when is_list(rule) do
+    [head | rest] = rule
+    head_rule = rules[head]
+
+    case head_rule do
+
+      s when is_binary(s) ->
+        if(String.starts_with?(message, s)) do
+          new_message = String.slice(message, 1..-1)
+          new_rule = rest
+          match_with_rule?(new_message, new_rule, rules)
+        else
+          false
+        end
+
+      l when is_list(l) -> # is always a list of lists 
+        l
+        |> Stream.map(fn partial_rule ->
+          (partial_rule ++ rest)
+        end)
+        |> Enum.any?(fn new_rule ->
+          match_with_rule?(message, new_rule, rules)
+        end)
+
+    end
+  end
+
   def parse_input_file(lines) do
     [rules_lines, messages] = lines
     |> Enum.chunk_by(fn l -> l == "" end)
--- test/advent19_test.exs
+++ test/advent19_test.exs
@@ -1,6 +1,42 @@
 defmodule Advent19Test do
   use ExUnit.Case
 
+  test "count matches with rule" do
+    rules = %{
+      0 => [[4,1,5]],
+      1 => [[2,3],[3,2]],
+      2 => [[4,4],[5,5]],
+      3 => [[4,5],[5,4]],
+      4 => "a",
+      5 => "b"
+    }
+    messages = ["ababbb", "bababa", "abbbab", "aaabbb", "aaaabbb"]
+
+    assert Advent19.count_matches_with_rule(messages, 0, rules) == 2
+  end
+
+  test "match message against rules" do
+    rules = %{
+      0 => [[4,1,5]],
+      1 => [[2,3],[3,2]],
+      2 => [[4,4],[5,5]],
+      3 => [[4,5],[5,4]],
+      4 => "a",
+      5 => "b"
+    }
+
+    # can't match
+    assert Advent19.match_with_rule?("bababa", 0, rules) == false
+    assert Advent19.match_with_rule?("aaabbb", 0, rules) == false
+    # matches with missing or too much elements
+    assert Advent19.match_with_rule?("abba", 0, rules) == false
+    assert Advent19.match_with_rule?("aaaabbb", 0, rules) == false
+    # matches
+    assert Advent19.match_with_rule?("ababbb", 0, rules) == true
+    assert Advent19.match_with_rule?("abbbab", 0, rules) == true
+    assert Advent19.match_with_rule?("aaaabb", 0, rules) == true
+  end
+
   test "parse input file lines" do
     input_file_content = """
     0: 4 1 5
